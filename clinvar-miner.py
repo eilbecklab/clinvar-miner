d006eadbbfe4f668bfd746edf9be6c118ed511d5
Alex Henrie <alex.henrie@utah.edu>
2017-11-10 14:48:47 -0700

    Display "unspecified" if the submitter's country is missing

--- clinvar-miner.py
+++ clinvar-miner.py
@@ -388,7 +388,7 @@ def template_functions():
     def submitter_tagline(submitter_info, submitter_primary_method):
         tagline = '<div class="tagline">'
         if 'country_name' in submitter_info:
-            tagline += 'Location: ' + submitter_info['country_name'] + ' &mdash; '
+            tagline += 'Location: ' + (submitter_info['country_name'] or 'unspecified') + ' &mdash; '
         tagline += 'Primary collection method: ' + submitter_primary_method
         tagline += '</div>'
         return tagline
