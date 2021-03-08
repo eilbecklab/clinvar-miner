309faf34297591e20cdb17604b1b3c0fa48d82ed
Alex Henrie <alex.henrie@utah.edu>
2021-03-08 16:51:49 -0700

    Add table of common country names and their 3-letter codes

--- get-submitter-info.py
+++ get-submitter-info.py
@@ -3,6 +3,33 @@
 import csv
 from pycountry import countries
 
+additional_common_names = {
+    'iran': 'IRN',
+    'palestine': 'PSE',
+    'north korea': 'PRK',
+    'russia': 'RUS',
+    'south korea': 'KOR',
+    'syria': 'SYR',
+}
+
+def lookup_country_code(country):
+    country = country.strip()
+    if not country:
+        return ''
+
+    # Try the pycountry database first
+    try:
+        return countries.lookup(country).alpha_3
+    except LookupError:
+        pass
+
+    # If that fails, try common names that are not in the pycountry database
+    country = country.lower()
+    if country in additional_common_names:
+        return additional_common_names[country]
+
+    return ''
+
 submitter_info = {}
 for row in csv.reader(open('submitter_info.tsv'), delimiter='\t'):
     submitter_id = row[0]
@@ -15,10 +42,7 @@ with open('organization_summary.txt') as f:
     for row in csv.reader(f, delimiter='\t'):
         submitter_name = row[0]
         submitter_id = row[1]
-        try:
-            country_code = countries.lookup(row[4]).alpha_3
-        except LookupError: #not a real country
-            country_code = ''
+        country_code = lookup_country_code(row[4])
         if not country_code and submitter_id in submitter_info:
             country_code = submitter_info[submitter_id][1]
         submitter_info[submitter_id] = [submitter_name, country_code]
