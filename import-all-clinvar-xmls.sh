8393163bec8873f54390eef1240c071fb2dc786b
Alex Henrie <alexhenrie24@gmail.com>
2022-07-14 20:24:24 -0600

    Look for each XML file in both the root directory and in the archive
    It seems that ClinVar stopped moving old releases to the archive
    directory in 2020, but they might start moving files there again in the
    future.

--- import-all-clinvar-xmls.sh
+++ import-all-clinvar-xmls.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
 function import {
-    url=$1
-    filename=$2
+    year=$1
+    month=$2
+
+    filename=ClinVarFullRelease_$year-$month.xml
+
+    # Try downloading from the root directory first
+    url=https://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/$filename.gz
     echo Downloading $url
     curl $url | gunzip > $filename 2> /dev/null
+
+    # If that fails, try downloading from the archive directory
+    if [ ! -s $filename ]; then
+        url=https://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/archive/$year/$filename.gz
+        echo Downloading $url
+        curl $url | gunzip > $filename 2> /dev/null
+    fi
+
+    # If we successfully got the file, import it!
     if [ -s $filename ]; then
         ./import-clinvar-xml.py $filename
     fi
+
+    # Delete the file and output a blank line
     rm $filename
     echo
 }
 
 # Import only the December release from past years
 for year in $(seq 2012 $(expr $(date +%Y) - 1)); do
-    filename=ClinVarFullRelease_$year-12.xml
-    import https://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/archive/$year/$filename.gz $filename
+    import $year 12
 done
 
 # Import the releases from every month of the current year
 year=$(date +%Y)
 for month in $(seq -f '%02g' 1 $(date +%m)); do
-    filename=ClinVarFullRelease_$year-$month.xml
-    import https://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/$filename.gz $filename
+    import $year $month
 done
